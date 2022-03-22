5f902454a95a06337a64cb9f9ee854f28245ae6f
Aliaksei Kvitsinski <aleksei.wm@gmail.com>
2022-03-22 16:19:13 +0100

    init

---
+++ tap_dashboard_reports/streams.py
@@ -0,0 +1,68 @@
+import json
+import requests
+from singer_sdk import typing as th
+from singer_sdk import Stream
+
+
+class ReportStream(Stream):
+    def __init__(self, tap=None, report=None):
+        self.report = report
+        self.query = self._load_query()
+        self.dimensions = list(
+            map(lambda v: v["dimension"].lower(), self.query["variables"]["groupBy"])
+        )
+        self.measures = list(
+            map(lambda v: v["id"].lower(), self.query["variables"]["measures"])
+        )
+        super().__init__(tap=tap)
+
+    @property
+    def name(self):
+        """Return primary key dynamically based on user inputs."""
+        return self.report["stream"]
+
+    @property
+    def primary_keys(self):
+        """Return primary key dynamically based on user inputs."""
+        return self.report.get("key_properties") or self.dimensions
+
+    @property
+    def schema(self) -> dict:
+        """Dynamically detect the json schema for the stream.
+        This is evaluated prior to any records being retrieved.
+        """
+
+        properties = [
+            th.Property(field, th.StringType)
+            for field in (self.dimensions + self.measures)
+        ]
+
+        # Return the list as a JSON Schema dictionary object
+        return th.PropertiesList(*properties).to_dict()
+
+    def _load_query(self):
+        with open(self.report["query"]) as f:
+            return json.load(f)
+
+    def _send_request(self):
+        headers = {
+            "Authorization": f"Bearer {self.config.get('auth_token')}",
+            "Content-type": "application/json",
+            "Accept": "text/plain",
+        }
+
+        auth_response = requests.post(
+            self.config.get("api_url"), headers=headers, data=json.dumps(self.query)
+        )
+
+        response = auth_response.json()
+        return response["data"]
+
+    def get_records(self, context):
+        columns = self.dimensions + self.measures
+        data = self._send_request()
+
+        for row in data["analytics"]["richStats"]["stats"]:
+            values = list(map(lambda x: x["value"], row))
+            record = dict(zip(iter(columns), iter(values)))
+            yield record
---
+++ tap_dashboard_reports/tap.py
@@ -0,0 +1,45 @@
+"""MySourceName tap class."""
+
+
+from typing import List
+from singer_sdk import Tap, Stream
+from singer_sdk import typing as th  # JSON schema typing helpers
+
+from tap_dashboard_reports.streams import ReportStream
+
+class TapDashboardReports(Tap):
+    """DashboardReports tap class."""
+
+    name = "tap-dashboard-reports"
+
+    # TODO: Update this section with the actual config values you expect:
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "auth_token",
+            th.StringType,
+            required=True,
+            description="The token to authenticate against the API service",
+        ),
+        th.Property(
+            "start_date",
+            th.DateTimeType,
+            description="The earliest record date to sync",
+        ),
+        th.Property(
+            "api_url",
+            th.StringType,
+            required=True,
+            default="https://api.mysample.com",
+            description="The url for the API service",
+        ),
+    ).to_dict()
+
+    def discover_streams(self) -> List[Stream]:
+        """Return a list of discovered streams."""
+        return [
+            ReportStream(tap=self, report=report)
+            for report in self.config.get("reports")
+        ]
+
+
+cli = TapDashboardReports.cli
