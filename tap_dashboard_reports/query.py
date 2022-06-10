6c2758369b8ac5ee586de2e7639fa2cc96e63c9b
Zviad <43080005+Zviad1990@users.noreply.github.com>
2022-06-10 11:26:26 +0300

    [SD-34] Add Chardonnay RETENTION RATE and ROAS OKRs day -30 (#1)
    * add last_sunday def
    
    * - fix typo
    - add variables
    
    * - add check variable  key_properties
    - include primary key to records
    
    * - fix accordance to comment
    
    * - erase space's
    - add custom_key flag
    -  change If statement for primary key
    
    * -black formating
    
    * change if statement custom key
    
    * - change logic for both custom key and dimensions

--- tap_dashboard_reports/query.py
+++ tap_dashboard_reports/query.py
@@ -1,10 +1,12 @@
-from datetime import date
+from datetime import date, timedelta
 from jinja2 import Environment
 from dateutil.relativedelta import relativedelta
 
 def _shift_date(d, interval, period):
     return d + relativedelta(**{interval: period})
 
+def _last_sunday(d):
+    return d - timedelta((d.weekday() + 1) % 7)
 
 def _format_date(d):
     try:
@@ -18,6 +20,7 @@ def render_query(template, **variables):
 
     variables = {
         "shift_date": _shift_date,
+        "last_sunday":_last_sunday,
         "format_date": _format_date,
         "start_date": date.today(),
         "end_date": date.today(),
--- tap_dashboard_reports/streams.py
+++ tap_dashboard_reports/streams.py
@@ -7,11 +7,13 @@ from tap_dashboard_reports.query import render_query
 from tap_dashboard_reports.client import Client
 from tap_dashboard_reports.period import get_iterator
 
+
 class ReportStream(Stream):
     def __init__(self, tap=None, report=None):
         self._report = report
         self._query_template = self._load_query_template()
         self._query_params = self._fetch_query_params()
+        self._custom_key = self._report.get("key_property", None)
 
         self._dimensions = list(
             map(
@@ -31,10 +33,13 @@ class ReportStream(Stream):
 
     @property
     def primary_keys(self):
-        """Return primary key dynamically based on user inputs."""
-        return self._report.get(
-            "key_properties", self._dimensions
-        )  # or self._dimensions
+        """Return primary key dynamically based on user inputs.
+        If in config doesn't have key key_property then take _dimensions
+        """
+        if self._custom_key is None:
+            return self._dimensions
+        else:
+            return [self._custom_key, *self._dimensions]
 
     # @property
     # def replication_key(self):
@@ -49,19 +54,20 @@ class ReportStream(Stream):
         """Dynamically detect the json schema for the stream.
         This is evaluated prior to any records being retrieved.
         """
-
         properties = [
             th.Property(field, th.StringType)
             for field in (self._dimensions + self._measures)
         ]
-
+        if self._custom_key is not None:
+            properties.append(th.Property(self._custom_key, th.StringType))
         # Return the list as a JSON Schema dictionary object
         return th.PropertiesList(*properties).to_dict()
 
     def get_records(self, context):
         columns = self._dimensions + self._measures
+        if self._custom_key is not None:
+            columns.append(self._custom_key)
         data = self._fetch_all_reports()
-
         for row in data:
             values = list(map(lambda x: x.get("value") or x.get("name"), row))
             record = dict(zip(iter(columns), iter(values)))
@@ -72,24 +78,25 @@ class ReportStream(Stream):
             return f.read()
 
     def _fetch_query_params(self):
-        query = render_query(
-            self._query_template,
-            **self._report.get("vars", {})
-        )
-
+        query = render_query(self._query_template, **self._report.get("vars", {}))
         return json.loads(query)
 
     def _fetch_all_reports(self):
         client = Client(self.config)
         with ThreadPoolExecutor(max_workers=10) as executor:
             tasks = []
-            for start_date, end_date in get_iterator(self.config, self._report).iterate():
-                tasks.append(executor.submit(self._fetch_one_report, client, start_date, end_date))
+            for start_date, end_date in get_iterator(
+                self.config, self._report
+            ).iterate():
+                tasks.append(
+                    executor.submit(
+                        self._fetch_one_report, client, start_date, end_date
+                    )
+                )
 
             data = []
             for t in tasks:
                 data += t.result()
-
         return data
 
     def _fetch_one_report(self, client, start_date, end_date):
@@ -97,16 +104,22 @@ class ReportStream(Stream):
             self._query_template,
             start_date=start_date,
             end_date=end_date,
-            **self._report.get("vars", {})
+            **self._report.get("vars", {}),
         )
-
-        return client.send(query)
+        result = client.send(query)
+        if self._custom_key is not None:
+            for data in result:
+                pk = {
+                    "id": None,
+                    "__typename": self._custom_key,
+                    "value": end_date.strftime("%Y-%m-%d"),
+                }
+                data.append(pk)
+        return result
 
 
 def _prepare_field_name(param):
     name = param["id"].lower()
-
     if param["day"] is not None:
         name = f'{name}_{param["day"]}'
-
     return name
